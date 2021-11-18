c429a134c3cd5ab38faed093bc12be786407649e
Sebastián Sánchez <ssp013sanchez@gmail.com>
2021-11-18 01:43:16 -0300

    New formulario with Material UI
    New formulario with Material UI

--- src/components/pages/Formulario/FormularioAlumno.jsx
+++ src/components/pages/Formulario/FormularioAlumno.jsx
@@ -1,185 +1,142 @@
-import React,{useState} from 'react'
-/* import Select from "react-select" */
-import { useForm } from "react-hook-form";
-import Axios from 'axios'
-import { Fragment } from 'react'
-const defaultValues = {
-    select: "",
-  };
+import React,{useState,Fragment} from 'react'
+import Button from "@material-ui/core/Button";
+import {Input,Card,CardContent,Typography,TextField,Grid,Autocomplete  } from "@mui/material";
 const FormularioAlumno = () => {
-  const endPoint ="http://localhost:10000/ucn/formulario"
-  //State
-  const [formulario, setFormulario] = useState({
-    rut: '',
-    nombre: '',
-    correo: '',
-    carrera: '',
-    cantidad: 0,
-    electivo1: '',
-    electivo2: '',
-    electivo3: '',
-  })  
-  const handleInputChange = (event) => {
-      setFormulario({
-            ...formulario, 
-            [event.target.name] : event.target.value
-        }) 
-        console.log(formulario)
-    }
-    //método de enviar 
-    const enviarDatos = (event) => {
-        event.preventDefault()
-        const numero = parseInt(formulario.cantidad)
-        formulario.cantidad = numero
-        //console.log('enviando datos...' + formulario.rut)
-        const sendJson = JSON.stringify(formulario,['rut','nombre','correo','carrera','cantidad','electivo1','electivo2','electivo3'])
-        console.log(sendJson)
-        //Método Post
-        Axios.post(endPoint,sendJson)
-        .then(res =>{
-            console.log(res.data)
-        })
-        event.target.reset()
-    }
-    //UserForm
-    const {control, register, handleSubmit, formState:{ errors } } =  useForm({defaultValues});
-    const onSubmit = (data, e)=>{
-        e.target.reset()
-    }
-    //Método Post
+    const endPoint ="http://localhost:10000/ucn/formulario"
+    //State
+    const [formulario, setFormulario] = useState({
+      rut: '',
+      nombre: '',
+      correo: '',
+      carrera: '',
+      cantidad: 0,
+      electivo1: '',
+      electivo2: '',
+      electivo3: '',
+    })
+ 
+    //Valores select
+    const cantidad = [
+        { label: '1', num: 1 },
+        { label: '2', num: 2 },
+        { label: '3', num: 3 }
+      ];
+    //Carreras que vienen desde la api
+    const carrera = [
+        { label: 'ICCI', sigla: 'ICCI' },
+        { label: 'ICI', sigla: 'ICI' },
+        { label: 'ITI', sigla: 'ITI' }
+      ];
+   //Electivos que vienen desde la api
+   const electivos = [
+    { label: 'Electivo 1', sigla: 'E1' },
+    { label: 'Electivo 2', sigla: 'E2' },
+    { label: 'Electivo 3', sigla: 'E3' }
+  ];
+  //Enviar datos
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-      
+  const handleChange = (event, value) => setSelectedOptions(value);
+  
+  const handleSubmit = () => console.log(selectedOptions);
+  
     return (
-            <Fragment>
-            <form onSubmit={enviarDatos}>
-            <div className="form-row">
-            <div class="form-group col-md-6">
-            <br/>
-            <input
-                    type="text"
-                    placeholder="Rut"
-                    class="form-control"
-                    name="rut"
-                    {...register("rut", { 
-                        required:{
-                            value: true,
-                            message: 'Campo requerido' 
-                        }
-                    })
-                    }
-                    onChange = {handleInputChange}
-            />
-            <span className="">
-                {errors.rut &&  errors.rut.message}
-            </span>
-            </div>
+        <Grid xs={12} sm={6}item>
+            <Typography gutterBottom variant ="h3" align="center" align="center">Formulario</Typography>
+            <form>
+                <Card >
+                    <CardContent>
+                        <Grid container spacing ={1}>
+                            <Grid xs={12} sm={6}item>
+                            <TextField label="Rut" placeholder="Ingrese Rut" name="rut" variant="outlined" fullWidth required></TextField>
+                            </Grid>
+                            <Grid xs={12} sm={6}item>
+                            <TextField label="Nombre" placeholder="Ingrese Nombre" name="nombre" variant="outlined" fullWidth required></TextField>
+                            </Grid>
+                            <Grid xs={12}item>
+                            <TextField type="email" label="Correo" placeholder="Ingrese alumnos.ucn.cl" name="correo" variant="outlined" fullWidth required></TextField>
+                            </Grid>
+                            <Grid xs={6}  item>
+                                <Autocomplete
+                                        
+                                        disablePortal
+                                        id="carrera"
+                                        options={carrera}
+                                       
+                                        onChange={(event, value) => console.log(value)} // prints the selected value
+                                        renderInput={params => (
+                                            <TextField {...params} label="Carrera" variant="outlined" fullWidth />
+                                        )}
+                                        required
+                                />
+                            </Grid>
+                            <Grid xs={6}  item>
+                                <Autocomplete
+                                       
+                                        disablePortal
+                                        id="cantidad"
+                                        options={cantidad}
+                                   
+                                        onChange={(event, value) => console.log(value)} // prints the selected value
+                                        renderInput={params => (
+                                            <TextField {...params} label="Cantidad" variant="outlined" fullWidth />
+                                        )}
+                                        required
+                                />
+                            </Grid>
+                            <Grid xs={12}  item>
+                            <Autocomplete
+                                        disablePortal
+                                        id="electivo1"
+                                        options={electivos}
+                                        
+                                        onChange={(event, value) => console.log(value)} // prints the selected value
+                                        renderInput={params => (
+                                            <TextField {...params} label="Electivo 1" variant="outlined" fullWidth />
+                                        )}
+                                        required
+                            />
+                            </Grid>
+                            <Grid xs={12} item>
+                            <Autocomplete
+                                        disablePortal
+                                        id="electivo2"
+                                        options={electivos}
+                                        
+                                        onChange={(event, value) => console.log(value)} // prints the selected value
+                                        renderInput={params => (
+                                            <TextField {...params} label="Electivo 2" variant="outlined" fullWidth />
+                                        )}
+                                        required
+                            />
+                            </Grid>
+                            <Grid xs={12} item>
+                            <Autocomplete
+                                        disablePortal
+                                        id="electivo3"
+                                        options={electivos}
+                                    
+                                        onChange={(event, value) => console.log(value)} // prints the selected value
+                                        renderInput={params => (
+                                            <TextField {...params} label="Electivo 3" variant="outlined" fullWidth />
+                                        )}
+                                        required
+                            />
+                            </Grid>
+                            <Grid xs={12} item>
+                            <Button  onClick={handleSubmit} type="submit" variant="contained" color="primary">Enviar</Button>
+                            
+                            </Grid>
+                        </Grid>
 
-            <div class="form-group col-md-6">
-            <br/>
-            <input
-                    type="text"
-                    placeholder="Nombre"
-                    class="form-control"
-                    name="nombre"
-                    {...register("nombre", { 
-                    required:{
-                        value: true,
-                        message: 'Campo requerido' 
-                    }
-                })}
-                    onChange = {handleInputChange}
-            />
-            <span className="">
-                {errors.rut &&  errors.rut.message}
-            </span>
-            </div>
 
-            <br/>
-            <input  
-                    placeholder="Correo"
-                    class="form-control"
-                    type="email"
-                    name="correo"
-                    {...register("correo", { 
-                    required:{
-                        value: true,
-                        message: 'Campo requerido' 
-                    }
-                })}
-                onChange={handleInputChange}   
-            />
-            <span className="">
-                {errors.correo &&  errors.correo.message}
-            </span>
-            <br/>
-            <div class="form-group col-md-6">  
-            <select {...register("Carrera",{ required: "Campo requerido" })} name="carrera" onChange={handleInputChange}>
-                
-                <option value="ICCI" >ICCI</option>
-                <option value="ICI">ICI</option>
-                <option value="ITI">ITI</option>
-                
-            </select>
-            <span className="">
-                {errors.Carrera &&  errors.Carrera.message}
-            </span>
-            </div>
-            <br/>
-            <div class="form-group col-md-6">  
-            <select  {...register("Cantidad",{ required: "Campo requerido"})}
-            onChange={handleInputChange} 
-            name="cantidad">
-                <option value={1} type="number">1</option>
-                <option value={2} type="number">2</option>
-                <option value={3} type="number">3</option>
-            </select>
-            <span className="">
-                {errors.Cantidad &&  errors.Cantidad.message}
-            </span>
-            </div>
-            <br/>
-            <div class="form-group col-md-6">  
-            <select  {...register("electivo1",{ required: "Campo requerido"})}
-            onChange={handleInputChange} >
-                <option value="E1" >Electivo 1</option>
-                <option value="E2">Electivo 2</option>
-                <option value="E3">Electivo 3</option>
-            </select>
-            <span className="">
-                {errors.ESele1 &&  errors.ESele1.message}
-            </span>
-            </div>
-            <br/>
-            <div class="form-group col-md-6">  
-            <select  {...register("electivo2",{ required: "Campo requerido"})}
-            onChange={handleInputChange} >
-                <option value="E1" >Electivo 1</option>
-                <option value="E2">Electivo 2</option>
-                <option value="E3">Electivo 3</option>
-            </select>
-            <span className="">
-                {errors.ESele2 &&  errors.ESele2.message}
-            </span>
-            </div>
-            <br/>
-            <div class="form-group col-md-6">  
-            <select  {...register("electivo3",{ required: "Campo requerido"})}
-            onChange={handleInputChange} >
-                <option value="E1" >Electivo 1</option>
-                <option value="E2">Electivo 2</option>
-                <option value="E3">Electivo 3</option>
-            </select>
-            <span className="">
-                {errors.ESele3 &&  errors.Cantidad.message}
-            </span>
-            </div>            
-            <br/>
-        </div>
-            <button type="submit" class="btn btn-primary" >Agregar</button>
-        </form>
-        
-        </Fragment>
-        
-        );
+
+                    </CardContent>
+                </Card>
+            </form>
+        </Grid>
+
+    )
 }
 export default FormularioAlumno
+
