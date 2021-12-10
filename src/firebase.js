e4fb91772f118fb96a87783ace3964cc0edfc3ff
Sebastián Sánchez <ssp013sanchez@gmail.com>
2021-12-10 17:15:43 -0300

    f
    f

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
-import firebase from 'firebase/app'
+//import firebase from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import 'firebase/auth';
+//import 'firebase/auth';
 // Your web app's Firebase configuration
-const app = firebase.initializeApp({
+/* const app = firebase.initializeApp({
   apiKey: "AIzaSyAEKqtJyQ4E9l77kaTNGnRbyDdfzJvVWHw",
   authDomain: "sistema-electivos-auth.firebaseapp.com",
   projectId: "sistema-electivos-auth",
   storageBucket: "sistema-electivos-auth.appspot.com",
   messagingSenderId: "41892760780",
   appId: "1:41892760780:web:d3e9711d5d83e2f3a20669"
-});
+}); */
 //usar auth en toda nuestra app
-export const auth = firebase.auth();
+//export const auth = firebase.auth();
 //export
-export default app;
+//export default app;
